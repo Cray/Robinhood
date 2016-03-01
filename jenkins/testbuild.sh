696d37fbbd1c1dd07fc0555ece18a0cbfa37d16c
John Suykerbuyk <john.suykerbuyk@seagate.com>
2016-03-01 10:32:15 -0700

    Update default robinhood version from 1.0 to 3.0
    - Update gitignore

--- jenkins/testbuild.sh
+++ jenkins/testbuild.sh
@@ -3,7 +3,7 @@
 export BUILD_NUMBER=2
 export JP_NEO_RELEASE=osaint
 export JP_SCM_URL=http://es-gerrit.xyus.xyratex.com/robinhood
-export JP_VERSION=1.0
+export JP_VERSION=3.0
 export JP_NEO_ID="o.1.0"
 export JP_REPO=robinhood
 
