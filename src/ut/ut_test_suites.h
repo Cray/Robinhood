04661d0ee1b0f7d9b3021ad99a4db950691b4864
Mikhail Pridushchenko <mikhail.v.pridushchenko@seagate.com>
2016-06-30 05:21:44 -0700

    Make robinhood unit-test-capable
    Issue: CASTOR-1614 Add unit tests to robinhood
    
    Implement:
    - Add CUnit to build definitions.
    - Add template test runner program.
    - Update RPM build settings to ignore unit tests.
    - Define 'test' make target.
    - Fix RPM .spec for Jenkins build.
    
    Change-Id: Id856f652f70555c76337beef12bda78a0f284f1c

---
+++ src/ut/ut_test_suites.h
@@ -0,0 +1,31 @@
+/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=4:tabstop=4:
+ */
+/*
+ * Copyright (C) 2016 Seagate Technology LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the CeCILL License.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license (http://www.cecill.info) and that you
+ * accept its terms.
+ */
+
+#ifndef UT_TEST_SUITES_H
+#define UT_TEST_SUITES_H
+
+/**
+ * @file ut_test_suites.h
+ * Declare test suites and list them for test suite registry.
+ * @attention To be included in main unit test runner file only!
+ */
+
+/**
+ * Test suites list.
+ */
+CU_SuiteInfo suites[] = {
+	CU_SUITE_INFO_NULL,
+};
+
+#endif
